33354f60e35012a5c923a6edd879bc712649416c
priyanka1984 <335975@cognizant.com>
2023-01-09 16:20:18 +0530

    user detail showing on click of user

--- src/app/userdetails.component.ts
+++ src/app/userdetails.component.ts
@@ -1,10 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { users } from './users';
 
 @Component({
   selector: 'user-details',
   templateUrl: './userdetails.component.html',
   styleUrls: ['./userdetails.component.scss'],
 })
-export class UserDetailsComponent {
-  constructor() {}
+export class UserDetailsComponent implements OnInit {
+  public user;
+
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit() {
+    const routeParams = this.route.snapshot.paramMap;
+    const userIdFromRoute = Number(routeParams.get('userId'));
+    this.user = users.find((user) => user.id === userIdFromRoute);
+  }
 }
